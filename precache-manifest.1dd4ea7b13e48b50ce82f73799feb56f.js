b1c2c730a9f1eeafd5440d03a9058f14647031f4
cyborg48 <cyborg414781@gmail.com>
2020-08-16 11:34:59 -0400

    Updates

---
+++ precache-manifest.1dd4ea7b13e48b50ce82f73799feb56f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df21655e1649cf0fd2e8545acb78193",
+    "url": "/redistrict/index.html"
+  },
+  {
+    "revision": "486a71ff622071690221",
+    "url": "/redistrict/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "387294ab69d8ecf4dd0a",
+    "url": "/redistrict/static/css/main.8fc207b4.chunk.css"
+  },
+  {
+    "revision": "486a71ff622071690221",
+    "url": "/redistrict/static/js/2.9539c458.chunk.js"
+  },
+  {
+    "revision": "a3c42654f0e35c13b47007c3f41517ac",
+    "url": "/redistrict/static/js/2.9539c458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "387294ab69d8ecf4dd0a",
+    "url": "/redistrict/static/js/main.45591ba0.chunk.js"
+  },
+  {
+    "revision": "0a63fca70eb9c716f28c",
+    "url": "/redistrict/static/js/runtime-main.15bc8b61.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/redistrict/static/media/logo.5d5d9eef.svg"
+  }
+]);
